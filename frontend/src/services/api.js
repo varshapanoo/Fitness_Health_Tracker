560d73a9686890cd73db0b38250ec0627c0c78cc
varshapanoo <varshapanoo@gmail.com>
2026-08-03 10:43:31 +0530

    Update frontend API URL for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL:"https://fitness-health-tracker.onrender.com/api",
+    baseURL:"https://fitness-health-tracker-p8g9.onrender.com/api",
 });
 
 export default API;
